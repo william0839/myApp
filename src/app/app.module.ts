16f41bd8d7cad6b1657cbfeb3c0f67b35b957fee
*McXZZvthjZ <long.guosong@bestskip.com>
2023-07-04 14:04:41 +0900

    first commmit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CalendarModule } from 'primeng/calendar';
+import { TooltipModule } from 'primeng/tooltip';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,6 +13,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    CalendarModule,
+    FormsModule,
+    TooltipModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
